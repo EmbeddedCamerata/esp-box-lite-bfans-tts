e0ad7aba43250cbfb61e43bc43e7c49a61ed1d95
KafCoppelia <k740677208@gmail.com>
2023-07-15 00:19:26 +0800

    ⚡️ enhancement: update `app_main`

--- main/main.c
+++ main/main.c
@@ -1,5 +1,6 @@
 #include "nvs_flash.h"
 #include "esp_wifi.h"
+#include "freertos/portmacro.h"
 
 /* BSP lib */
 // #include "bsp_lcd.h"
@@ -15,10 +16,11 @@
 #include "https_req.h"
 #include "tts_report.h"
 
+extern char https_req_buf[MAX_REQUEST_BUF_LEN];
+static void tts_welcome_task(void *pvParams);
+
 void app_main(void)
 {
-    char *bfans;
-
     ESP_ERROR_CHECK(bsp_board_init());
     ESP_ERROR_CHECK(bsp_board_power_ctrl(POWER_MODULE_AUDIO, true));
 
@@ -33,6 +35,9 @@ void app_main(void)
 
     ESP_ERROR_CHECK(tts_init());
 
+    // Connect to WIFI
+    wifi_init_sta();
+
     // LVGL init
     // ESP_ERROR_CHECK(lv_port_init());
     // bsp_lcd_set_backlight(true);
@@ -40,8 +45,16 @@ void app_main(void)
     // TODO LVGL显示任务
     // 消息队列传递从HTTPS得到并cJSON解析后的数据至显示任务
 
-    // Connect to WIFI
-    wifi_init_sta();
+    xTaskCreate(&tts_welcome_task, "tts_welcome_task", 4 * 1024, NULL, 5, NULL);
+    xTaskCreate(&https_request_task, "https_request_task", 8 * 1024, NULL, 5, NULL);
 
-    xTaskCreate(&https_request_task, "https_request_task", 4 * 1024, NULL, 5, NULL);
+    /* Register buttons callback functions */
+    bsp_btn_register_callback(BOARD_BTN_ID_PREV, BUTTON_PRESS_DOWN, tts_report_cb, https_req_buf);
+    bsp_btn_register_callback(BOARD_BTN_ID_ENTER, BUTTON_PRESS_DOWN, tts_welcome_report_cb, NULL);
+}
+
+static void tts_welcome_task(void *pvParams)
+{
+    tts_welcome_report();
+    vTaskDelete(NULL);
 }
