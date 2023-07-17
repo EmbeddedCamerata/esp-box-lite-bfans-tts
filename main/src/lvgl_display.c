1a182fb6fe4f29036d00a71f49e274f0850f1a20
KafCoppelia <k740677208@gmail.com>
2023-07-17 23:45:29 +0800

    🚀 enhancement: change label text size

--- main/src/lvgl_display.c
+++ main/src/lvgl_display.c
@@ -9,8 +9,9 @@ void lvgl_display_init_task(void *pvParams)
 
     /* Create a white label, set its text and align it to the center */
     label = lv_label_create(lv_scr_act());
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_24, 0); // Font size 24
     lv_label_set_text(label, "Followers: 0");
-    lv_obj_set_style_text_color(lv_scr_act(), lv_palette_main(LV_PALETTE_PINK), LV_PART_MAIN);
+    lv_obj_set_style_text_color(lv_scr_act(), lv_palette_main(LV_PALETTE_ORANGE), LV_PART_MAIN);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
     vTaskDelete(NULL);
